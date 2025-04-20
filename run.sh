9b428014122f1de885ccb47ddd63d904a63c40fb
anhsky <79846249+anhsky@users.noreply.github.com>
2025-04-21 02:47:46 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-set -ex
+set -e
 
-# Cài đặt các phụ thuộc cần thiết
+# Cài đặt dependencies
 apt-get update
 apt-get install -y wget tar xz-utils lib32gcc-s1
 
-# Tải FiveM
-ARTIFACT="5848-4a1ad37603deb75b31d32a5c911fb8b34f6b5d3a"
-wget -q https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/${ARTIFACT}/fx.tar.xz
+# Tải FiveM (phiên bản cố định ổn định)
+wget https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/5848-4a1ad37603deb75b31d32a5c911fb8b34f6b5d3a/fx.tar.xz
 
 # Giải nén và chạy
 tar xf fx.tar.xz
